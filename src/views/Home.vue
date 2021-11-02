dd323dc6c18d5c0ad356b3d39800cc754c46ab15
David <62397679+MacDavid1989@users.noreply.github.com>
2021-11-02 12:26:07 -0400

    update interface, map response and push to ref, iterate over forecast and display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="home">
-    <h1>{{ forecast }}</h1>
+  <div class="home" v-for="day in forecast" :key="day.dt">
+    <h1>{{ day.dt }}</h1>
+    <p>{{ day.weather[0].description }}</p>
+    <span>{{ day.temp.max }} - {{ day.temp.min }}</span>
   </div>
 </template>
 
@@ -8,9 +10,17 @@
 import { defineComponent, ref } from "vue";
 
 interface Forecast {
-  daily: [];
+  dt: number;
+  temp: {
+    min: number;
+    max: number;
+  };
+  weather: [
+    {
+      description: string;
+    }
+  ];
 }
-
 export default defineComponent({
   name: "Home",
   setup() {
@@ -20,11 +30,15 @@ export default defineComponent({
 
     const load = async () => {
       try {
-        let data = await fetch(
-          "https://api.openweathermap.org/data/2.5/onecall?lon=-79.4163&lat=43.7001&units=metric&exclude=minutely,hourly,alerts&appid=64aa619e2643a5c124b17c1a72923430"
-        );
+        let data = await (
+          await fetch(
+            "https://api.openweathermap.org/data/2.5/onecall?lon=-79.4163&lat=43.7001&units=metric&exclude=minutely,hourly,alerts&appid=64aa619e2643a5c124b17c1a72923430"
+          )
+        ).json();
 
-        forecast.value = await data.json();
+        data.daily.map((day: Forecast, index: number) => {
+          if (index < 5) forecast.value.push(day);
+        });
       } catch (err) {
         console.error(err);
       }
